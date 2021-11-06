eb6eafa6589f5815432d91b74e47478f1054d71c
Daguerre45 <eldeprueba45@gmail.com>
2021-11-06 13:43:52 +0100

    Ejercicio completo con algunos extras

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,7 +1,45 @@
 package aplicacion;
 
-public class Principal {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Principal {
+
+    public static void main(String[] args) {
+        int numeroUsuario;
+        int numeroAleatorio;
+        int numeroAdivinar;
+        int casos = 0;
+
+	Scanner sc = new Scanner(System.in);
+	try{
+        	System.out.println("Introzuca un número máximo mayor que 1 ");
+        	numeroUsuario = sc.nextInt();
+        	if (numeroUsuario <= 1) {
+            		System.out.println("Introduzca un número mayor que 1");
+            		numeroUsuario = sc.nextInt();
+        	}
+
+		numeroAleatorio = (int) (Math.random() * numeroUsuario + 1);
+
+        	System.out.println("Ahora deberá adivinar el numero generado aleatoriamente entre 1 y " 
+				+ numeroUsuario);
+
+        	numeroAdivinar = sc.nextInt();
+        	for (int i = 0; i <= numeroAleatorio; i++) {
+            		if (numeroAdivinar == numeroAleatorio) {
+                		System.out.println("¡Enorabuena has adivinado el número!");
+           		 } else if (numeroAdivinar < numeroAleatorio) {
+                		System.out.println("OHHH no era el número, prube con otro numero más grande");
+               		 	numeroAdivinar = sc.nextInt();
+           	 	} else if (numeroAdivinar > numeroAleatorio) {
+                		System.out.println("OHHH no era el número, prube con otro numero más pequeño");
+                		numeroAdivinar = sc.nextInt();
+            		}
+        	}
+        	sc.close();
+	}catch(Exception e){
+		System.out.println("Añada un NUMERO no una letra");
+	}
     }
+
 }
