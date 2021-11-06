62360c85765b9bc22ddb441ee7c94c3a4394169c
Daguerre45 <alberto.daguerretorres@usp.ceu.es>
2021-11-06 21:18:25 +0100

    Codigo mejorado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -4,42 +4,59 @@ import java.util.Scanner;
 
 public class Principal {
 
-    public static void main(String[] args) {
-        int numeroUsuario;
-        int numeroAleatorio;
-        int numeroAdivinar;
-        int casos = 0;
+	public static void main(String[] args) {
+		int numeroUsuario;
+		int numeroAleatorio;
+		int numeroAdivinar;
+		int caso = 0;
+		String nombre;
 
-	Scanner sc = new Scanner(System.in);
-	try{
-        	System.out.println("Introzuca un número máximo mayor que 1 ");
-        	numeroUsuario = sc.nextInt();
-        	if (numeroUsuario <= 1) {
-            		System.out.println("Introduzca un número mayor que 1");
-            		numeroUsuario = sc.nextInt();
-        	}
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Hola, añada su nombre");
+			nombre = sc.next();
+			System.out.println(nombre + " introzuca un número máximo mayor que 1 ");
+			numeroUsuario = sc.nextInt();
+			if (numeroUsuario <= 1) {
+				System.out.println(nombre + " introduzca un número mayor que 1");
+				numeroUsuario = sc.nextInt();
+			}
 
-		numeroAleatorio = (int) (Math.random() * numeroUsuario + 1);
+			numeroAleatorio = (int) (Math.random() * numeroUsuario + 1);
 
-        	System.out.println("Ahora deberá adivinar el numero generado aleatoriamente entre 1 y " 
-				+ numeroUsuario);
+			System.out.println(
+					nombre + " ahora deberá adivinar el numero generado aleatoriamente entre 1 y " + numeroUsuario);
 
-        	numeroAdivinar = sc.nextInt();
-        	for (int i = 0; i <= numeroAleatorio; i++) {
-            		if (numeroAdivinar == numeroAleatorio) {
-                		System.out.println("¡Enorabuena has adivinado el número!");
-           		 } else if (numeroAdivinar < numeroAleatorio) {
-                		System.out.println("OHHH no era el número, prube con otro numero más grande");
-               		 	numeroAdivinar = sc.nextInt();
-           	 	} else if (numeroAdivinar > numeroAleatorio) {
-                		System.out.println("OHHH no era el número, prube con otro numero más pequeño");
-                		numeroAdivinar = sc.nextInt();
-            		}
-        	}
-        	sc.close();
-	}catch(Exception e){
-		System.out.println("Añada un NUMERO no una letra");
+			System.out.println(nombre + " añade un número entre 1 y " + numeroUsuario);
+			numeroAdivinar = sc.nextInt();
+
+			while (numeroAdivinar != numeroAleatorio) {
+				switch (caso) {
+				case 0:
+					System.out.println(nombre + " añada otro número");
+					numeroAdivinar = sc.nextInt();
+					if (numeroAdivinar < numeroAleatorio) {
+						caso = 1;
+					}
+					if (numeroAdivinar > numeroAleatorio) {
+						caso = 2;
+					}
+					break;
+				case 1:
+					System.out.println(nombre + " el numero a adivinar es más MAYOR");
+					caso = 0;
+					break;
+				case 2:
+					System.out.println(nombre + " el numero a adivinar es más PEQUEÑO");
+					caso = 0;
+					break;
+				}
+			}
+			System.out.println("Enorabuena " + nombre + " adivaste el número");
+			sc.close();
+		} catch (Exception e) {
+			System.out.println("Añade un NUMERO no una letra");
+		}
 	}
-    }
 
 }
